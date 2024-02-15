4c2e92ea48fb3dbc39affd78544bdd0ec2550755
Lyria-chan <maja.knaflewska@gmail.com>
2024-02-15 21:56:15 +0100

    reworked and fixed a lot + added a local "database", should be at least 20 times faster now.

--- PPDTargetScoreDownloader.py
+++ PPDTargetScoreDownloader.py
@@ -2,23 +2,37 @@ import requests, pykakasi, json
 from time import time
 import dllPPD as dll
 
+dldirectory = dll.callPath()
+if 'songs' in dldirectory:
+    dldirectory = dldirectory[:dldirectory.index('songs') + 6]
 
 # --------- # EDIT VALUES HERE # --------- #
-dldirectory = fr'{dll.callPath()}TARGET SCORES'
-    # the localization where the songs will be downloaded. write it inside of the  r' '. DO NOT DELETE THE R, THE PROGRAM WILL NOT WORK
+dldirectory = fr'{dldirectory}TARGET SCORES'
+    # the localization where the songs will be downloaded. write it inside of the  fr' '. DO NOT DELETE THE 'fr', THE PROGRAM WILL NOT WORK
 videoquality = 1
     # highest video quality the program will attempt to download from youtube. 0 is 1080p, 1 is 720p, 5 is 144p etc.
-
+startfrom = 0
+    # start from this position in the song list, use if download was interrupted
 # ---------------------------------------- #
 
 
 # here is the rest of the code. good luck analyzing it if you want to
 kks = pykakasi.kakasi()
+def addIDToTxt(id, name):
+    # open file specified in path /assets/{name}.txt
+    with open(f'assets/{name}.txt', 'a') as f:
+        f.write('\n' + str(id))
+
+    
+
+
+
+    pass
 
 def loadUrl(url, session):
     for attempts in range(10):
         try:
-            html=requests.get(url)
+            html=session.get(url)
             if html.status_code==200:
                 html = html.text
                 break
@@ -94,8 +108,9 @@ def updateSongs(song_list_new):
 
 song_list = loadList()
 
-with open("assets/target_scores.json","w") as song_list_old:
-    json.dump(song_list,song_list_old)
+# why is this here?? i shall find out someday
+# with open("assets/target_scores.json","w") as song_list_old:
+#     json.dump(song_list,song_list_old)
 
 csinput_keyword_list = [
     {'raw':'csinput','display':'CSInput'},
@@ -165,7 +180,7 @@ if newOnly:
 
 time_start = round(time(),2)
 
-with open('assets\log.txt','a') as log:
+with open('assets/log.txt','a') as log:
     log.write("ppdSongSearch log file")
     if mode == 1:
         log.write("\nSearching for CSInput")
@@ -179,53 +194,74 @@ with open('assets\log.txt','a') as log:
     log.write("\n\nSongs found:\n")
     
 with requests.Session() as session:
-    for song in song_list:
+    cs_local_ids = dll.refreshIdDatabase(fr'{dldirectory}TARGET SCORES', return_all = True)
+    no_cs_local_ids = open(r'assets/target_scores_no_csinput.txt','r').read().splitlines()
+
+
+    local_ids = cs_local_ids + no_cs_local_ids
+    real_checked_count = None
+    for song in song_list[startfrom:]:
         song_id = song['url'].split('/')[-1]
-        html = str(loadUrl(song['url'], session))
-        
-        # for keyword, keyword_not in zip(keyword_list, keyword_not_list):
-        #     print(keyword_list)
-        #     if keyword['raw'] in html.lower() and not keyword_not['raw'] in html.lower():
-        #         print(f"\rKeyword \"{keyword['display']}\" found in \"{song['name']}\", rank {checked_count+1}" + 20 * " " +
-        #               f"\nurl: {song['url']}" +
-        #               "\n- - - - -")
-        #         found_count += 1
-        #         with open('log.txt','a') as log:
-        #             log.write(f"{song['name']}, rank {checked_count+1}, {song['url']}\n")
-        
-        for keyword in keyword_list:
-            if keyword_not_list:
-                for keyword_not in keyword_not_list:
-                    if keyword['raw'] in html.lower() and not keyword_not['raw'] in html.lower():
-                        print(f"\rKeyword \"{keyword['display']}\" found in \"{song['name']}\", rank {checked_count+1}" + 20 * " " +
+        if song_id in local_ids:
+            # if real_checked_count != None:
+                # print(f"\r{song['name']} already downloaded, skipping...", end='')
+            rank = checked_count + 1 + startfrom
+            checked_count += 1
+        else:
+            if real_checked_count == None: real_checked_count = 0
+            html = str(loadUrl(song['url'], session))
+            
+            # for keyword, keyword_not in zip(keyword_list, keyword_not_list):
+            #     print(keyword_list)
+            #     if keyword['raw'] in html.lower() and not keyword_not['raw'] in html.lower():
+            #         print(f"\rKeyword \"{keyword['display']}\" found in \"{song['name']}\", rank {checked_count+1}" + 20 * " " +
+            #               f"\nurl: {song['url']}" +
+            #               "\n- - - - -")
+            #         found_count += 1
+            #         with open('log.txt','a') as log:
+            #             log.write(f"{song['name']}, rank {checked_count+1}, {song['url']}\n")
+            rank = checked_count + 1 + startfrom
+            old_found_count = found_count
+            for keyword in keyword_list:
+                if keyword_not_list:
+                    for keyword_not in keyword_not_list:
+                        if keyword['raw'] in html.lower() and not keyword_not['raw'] in html.lower():
+                            print(f"\rKeyword \"{keyword['display']}\" found in \"{song['name']}\", rank {rank}" + 20 * " " +
+                                  f"\nurl: {song['url']}" +
+                                  "\n- - - - -")
+                            found_count += 1
+                            with open('assets/log.txt','a') as log:
+                                log.write(f"{song['name']}, rank {rank}, {song['url']}\n")
+                            dll.LvDl(song_id, dldirectory, True, videoquality)
+                else:
+                    if keyword['raw'] in html.lower():
+                        print(f"\rKeyword \"{keyword['display']}\" found in \"{song['name']}\", rank {rank}" + 20 * " " +
                               f"\nurl: {song['url']}" +
                               "\n- - - - -")
                         found_count += 1
-                        with open('assets\log.txt','a') as log:
-                            log.write(f"{song['name']}, rank {checked_count+1}, {song['url']}\n")
-                        dll.LvDl(session, song_id, dldirectory, True, videoquality)
-            else:
-                if keyword['raw'] in html.lower():
-                    print(f"\rKeyword \"{keyword['display']}\" found in \"{song['name']}\", rank {checked_count+1}" + 20 * " " +
-                          f"\nurl: {song['url']}" +
-                          "\n- - - - -")
-                    found_count += 1
-                    with open('assets\log.txt','a') as log:
-                        log.write(f"{song['name']}, rank {checked_count+1}, {song['url']}\n")
-                    dll.LvDl(session, song_id, dldirectory, True, videoquality)
-        checked_count += 1
-        
-        time_remaining = (round(time() - time_start,2) / checked_count) * (len(song_list) - checked_count)
-        time_remaining = timeString(time_remaining)
+                        with open('assets/log.txt','a') as log:
+                            log.write(f"{song['name']}, rank {rank}, {song['url']}\n")
+                        dll.LvDl(song_id, dldirectory, True, videoquality)
+            checked_count += 1
+            real_checked_count+=1
+            if old_found_count == found_count:
+                addIDToTxt(song_id, "target_scores_no_csinput")
+
+
+            time_remaining = (round(time() - time_start,2) / real_checked_count) * (len(song_list) - checked_count)
+            time_remaining = timeString(time_remaining)
+                
+            progress = round(rank * 100 / len(song_list))
             
-        progress = round(checked_count * 100 / len(song_list))
+            print(f"\r{rank} checked, {found_count} found, {progress}% completed, {time_remaining} remaining" + 10 * " ",end='')
         
-        print(f"\r{checked_count} checked, {found_count} found, {progress}% completed, {time_remaining} remaining" + 10 * " ",end='')
-    
-    total_time = round(time() - time_start)
-    total_hr = str(total_time // 3600).zfill(2)
-    total_min = str((total_time % 3600) // 60).zfill(2)
-    total_sec = str(total_time % 60).zfill(2)
-    total_time = f"{total_hr}:{total_min}:{total_sec}"
-    
-    print(f"\rSearch completed in {total_time}. {checked_count} songs checked, {found_count} found." + 20 * " ")
+        total_time = round(time() - time_start)
+        total_hr = str(total_time // 3600).zfill(2)
+        total_min = str((total_time % 3600) // 60).zfill(2)
+        total_sec = str(total_time % 60).zfill(2)
+        total_time = f"{total_hr}:{total_min}:{total_sec}"
+    if found_count:
+        print(f"\rSearch completed in {total_time}. {checked_count} songs checked, {found_count} found." + 20 * " ")
+    else:
+        print("\rThere's no new ranked levels, you muffinhead. Go use the other tool to download more charts!" + 20 * " ")
+    input("Press enter to exit...")
