486f1b01e73a7ef3e4c4dfbf1db18efae3801af1
Lyria-chan <maja.knaflewska@gmail.com>
2024-06-20 22:12:42 +0200

    STILL IN TESTING, PATH CAN ONLY BE CHANGED IN CODE + MANY ERRORS - USE AT OWN RISK

--- PPDallLevelDownloader.py
+++ PPDallLevelDownloader.py
@@ -1,13 +1,21 @@
 import PPDLevelDownloader as downloader
 import dllPPD as dll
 
+authorwhitelist = ['siteswap', 'kazoo king', 'sach', 'blue star', 'palmtree_freak']
+authorblacklist = []
+
+def DlLoop(i, level):
+    i += 1
+    print(f"{i}. {level['title']} by {level['author'].strip()}")
+    print(f'--      {level['id']}       --')
+    dll.LvDl(level['id'], 'D:\\KHC\\PPD\\songs\\Creator packs\\' + level['author'].strip(), True, 0, level['video'], level['title'])
+    print(' ')
+    return i
+
 parsedinfo = downloader.handle('', 'all')
 i = 0
 for level in parsedinfo:
     if level['csinput'] == True:
-        dll.LvDl(level['id'], 'D:\\KHC\\PPD\\songs\\Creator packs\\' + level['author'].strip(), True, 0, level['video'], level['title'])
-        i += 1
-        print(f"{i}. {level['title']} by {level['author'].strip()}")
-        print(level['saved'])
-
-    
+        if authorwhitelist:
+            if level['author'].strip().lower() in authorwhitelist:
+                i = DlLoop(i, level)
