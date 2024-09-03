1afde3726124139ee672ec4cf652c4950fa849f2
Lyria-chan <maja.knaflewska@gmail.com>
2024-09-03 17:22:34 +0200

    changed name, added updating

--- setup/import-and-update-libraries.py
+++ setup/import-and-update-libraries.py
@@ -3,4 +3,4 @@ import subprocess
 li = ['lxml', 'eel', 'pykakasi', 'niconico-dl', 'pytubefix', 'gdown']
 
 for l_name in li:
-    subprocess.check_call(['pip', 'install', l_name])
+    subprocess.check_call(['pip', 'install', l_name, '--upgrade'])
